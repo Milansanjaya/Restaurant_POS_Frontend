8bbee446e325f1f30ae0a4a1d705b24aaf55af78
Milan sanjaya <sanjaya2170@gmail.com>
2026-04-09 08:43:19 +0530

    update

--- pos-frontend/src/api/axios.ts
+++ pos-frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "https://restaurantposbackend-production.up.railway.app/ap"
+  baseURL: import.meta.env.VITE_API_URL || "https://restaurantposbackend-production.up.railway.app/api"
 });
 
 api.interceptors.request.use((config) => {
